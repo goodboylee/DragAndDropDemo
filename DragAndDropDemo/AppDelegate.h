0e39251a2b3a7bc0b78ca45642d563a84019e089
lotus <goodboylee@126.com>
2020-01-02 16:18:47 +0800

    drag and drop demo using in iOS

---
+++ DragAndDropDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DragAndDropDemo
+//
+//  Created by lotus on 2020/1/2.
+//  Copyright © 2020 lotus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ DragAndDropDemo/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  DragAndDropDemo
+//
+//  Created by lotus on 2020/1/2.
+//  Copyright © 2020 lotus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
